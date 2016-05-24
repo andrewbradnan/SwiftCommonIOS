525b5ad26acdf22ad0462c1c4db20ca35b1a0129
Andrew Bradnan <andbr@microsoft.com>
2016-05-24 11:15:29 -0700

    Initial Commit

---
+++ SwiftCommonIOS/SwiftCommonIOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCommonIOS.h
+//  SwiftCommonIOS
+//
+//  Created by Andrew Bradnan on 5/24/16.
+//  Copyright © 2016 Belkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCommonIOS.
+FOUNDATION_EXPORT double SwiftCommonIOSVersionNumber;
+
+//! Project version string for SwiftCommonIOS.
+FOUNDATION_EXPORT const unsigned char SwiftCommonIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCommonIOS/PublicHeader.h>
+
+
